855633589fd9e2b252bf1937cbacd0f3ed4954fa
melsil11 <112329390+melsil11@users.noreply.github.com>
2023-06-11 17:03:50 -0400

    practiced multiple quotes

--- session1.py
+++ session1.py
@@ -1,5 +1,42 @@
 # Write a function that prints "Hello World!"
 print ("Hello World!")
-
+# Congratulate your self
 print ("Congratulations! You wrote your first Python program")
+# Print a number (interger) 
+print(7)
 
+# print a string, or series of character, as shown in the "Hello World" program
+print("I love python")
+print("dkjsa457387@fksd")
+
+# print the result of a mathmatical equation.
+print(22*2+1)
+
+# STRINGS
+
+# A string is a series of characters, which can include letters, numbers, and special characters such as exclamation points.
+
+# We can use the following formats to print a string: 1) single quotes (one apostrophe), 2) double quotes, or 3) triple single quotes (three apostrophes).
+
+# print('single quotes') 
+
+# print("double quotes") 
+
+# print('''triple single quotes''')
+
+# when we run the following code, all print functions will print the same result "Hello World"
+
+print('Hello world!') 
+print("Hello world!") 
+print('''Hello world!''')
+
+# Here is an example of when triple single quotes (three apostrophes) could be useful.
+# The quote includes a quotation in double quotes and an apostrophe for the word "You're".
+print('''Stephanie says, "You're amazing!"''')
+
+# Or we can use a backwards slash to escape an apostrophe (or double quotes).
+# This example uses apostrophes around the string and a backslash before the apostrophe in "You're."
+print('Stephanie says, "You\'re amazing!"')
+
+# Multiply a string by a number (integer).
+print('Hello' * 3)
