782411d9728567588ed7908be80f83d32fd6e761
melsil11 <112329390+melsil11@users.noreply.github.com>
2023-06-14 17:31:16 -0400

    finished strings

--- session1.py
+++ session1.py
@@ -40,3 +40,43 @@ print('Stephanie says, "You\'re amazing!"')
 
 # Multiply a string by a number (integer).
 print('Hello' * 3)
+
+# Here is an example of when triple single quotes (three apostrophes) could be useful.
+# The quote includes a quotation in double quotes and an apostrophe for the word "You're".
+print('''Stephanie says, "You're amazing!"''')
+
+# Or we can use a backwards slash to escape an apostrophe (or double quotes).
+# This example uses apostrophes around the string and a backslash before the apostrophe in "You're."
+print('Stephanie says, "You\'re amazing!"')
+# Practice
+print('Tell it to me stright')
+# Practice:
+print(' mel says "you\'re Amazing"')
+# Multiply a string by a number (integer).
+print('Hello' * 3)
+# HelloHelloHello
+# Practice:
+print('Hi' * 2)
+# HiHi
+# to print with spaces add a space after the word but before the closing quote
+print('hello ' * 3)
+
+# Check the character length of a string using the len() function.
+print(len('Hello!'))
+# Practice:
+print(len('Melanie Silva'))
+# it counts spaces as characters
+
+# We can also add together, or concatenate, more than one string.
+# We will be using concatenation often in future sessions.
+print("Hi, " + "Stephanie" + "!") 
+
+# Note the space after "Hi, " in this example and how the code executes.
+
+# practice:
+print('Hi, ' + 'Melanie' +'!' )
+# //////////////
+
+# //Variables//
+
+
