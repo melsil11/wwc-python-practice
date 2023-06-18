29ac6295e6b0bc6e040e3f3bfb6ba89778e08b40
melsil11 <112329390+melsil11@users.noreply.github.com>
2023-06-18 12:32:14 -0400

    refresher from session 1

---
+++ session2.py
@@ -0,0 +1,53 @@
+# Review/Refresher
+
+# **Comments**
+
+# Comments help us communicate the purpose of our code. Comments are "hidden" and will not execute or print out to the user when our code runs. Comments can start with a hashtag (#). This is an example of a comment.
+
+# **Print Function**
+
+# The print function will "print" (or show) data to the user.
+# The print function is written as print() with the data placed between the parentheses.
+
+print()
+
+# Examples:
+print(1)
+print(25.51)
+print('abc123!@#')
+print("hello")
+
+# Strings (str)
+
+# A string is a series of characters, which can include letters, numbers, and special characters.
+# 'A string using single quotes (one apostrophe)'
+# "A string using double quotes"
+# '''A string using triple single quotes (three apostrophes)'''
+
+# Write a function that prints "Hello world!" to the user.
+print('Hello world!')
+
+# **Variables**
+
+# Variables are pieces of memory that store our data.
+# When code runs into a variable, Python retrieves the value assigned to the variable for the function (or task) being executed. We create a variable by using an equal sign (=) to assign a value.
+
+# w = 290,000
+# women = 290,000
+# women_who_code = 290,000
+
+coding = "Yay!"
+print(coding)
+
+# **Integer (int)**
+
+# Integers are whole numbers, no decimal points. Can be positive or negative.
+# Examples: 3, 27, 28384723, -10, -234
+awesome_integer = -300
+print(awesome_integer)
+
+# **Floating Point Numbers (float)**
+
+# Floating point numbers, or floats, are numbers with decimal points. Can be positive or negative.Examples: 3.1415926, 1.0, 100.5, -5.5, -233.33
+awesome_float = 3.14
+print(awesome_float)
